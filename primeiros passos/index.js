78fad9f2ea87d023d03ceaa0890d08369d5e63a1
leonardo <leonardocave934@gmail.com>
2022-12-13 18:33:43 -0300

    number

---
+++ primeiros passos/index.js
@@ -0,0 +1,16 @@
+console.log("Ola tudo bom?")
+console.log("Sabia q Pão é bom?")
+
+//console.log("Leonardo ${1+1*1/1-1}")
+
+
+console.log(`Leonardo ${1+1*1/1-1}`)
+
+//console.log(Infinity)
+
+
+console.log(666)
+console.log(12.666 + 654)
+console.log(12.666 / "asdf")
+console.log(12.666 === Infinity)
+console.log(Infinity)
