8eeed9edb52d06513ffa04afb32786216c18a480
leonardo <leonardocave934@gmail.com>
2022-12-15 15:55:39 -0300

    Arrays

---
+++ primeiros passos/indexquatro.js
@@ -0,0 +1,24 @@
+
+
+
+const feline = [
+    'cheetah',
+    'Jaguar',
+    'leopard',
+    'cat',
+    'lion',
+    'Puma',
+    'eurasian lynx',
+    'tiger',
+    'serval',
+    'carcal',
+    'jaguarland',
+    'asian golden cat',
+    'clouded leopard',
+    'black leopard',
+    'pallas cat',
+    'snow leopard'
+]
+
+console.log(feline[7])
+
