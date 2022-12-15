54a20b8afc2b352832baa2fe206f4f0c95e6db88
leonardo <leonardocave934@gmail.com>
2022-12-15 16:48:13 -0300

    atividade

---
+++ exercicios/index.js
@@ -0,0 +1,42 @@
+
+// atividade 1,2,3,4,5,6
+
+let student = {
+
+     name: "Carlos",
+     age: 34,
+     weight: 43.1,
+     isSubscribed: true
+};
+
+// console.log(`o ${student.name} tem ${student.age} de idade, e pesa ${student.weight} kg `)
+
+
+// atividade 7
+
+// students = [
+//     student
+// ]
+
+// console.log(students[0])
+
+//atividade 8 
+
+const johni = {
+    name:"johni",
+    age:25,
+    weight:30.8,
+    isSubscribed: true,
+}
+
+students = [
+    student,
+    johni
+]
+students[1] = johni
+console.log (students)
+
+//atividade 9
+
+let a = 1
+console.log(a)
