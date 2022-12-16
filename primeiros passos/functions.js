a373a448e9d9e3c40bfa179a59caa9865c3b893d
leonardo <leonardocave934@gmail.com>
2022-12-16 20:45:59 -0300

    Retornando valores dentro da função

--- primeiros passos/functions.js
+++ primeiros passos/functions.js
@@ -1,12 +1,17 @@
+let total = 0
+
+
 const sum = function(number1, number2) {
-    console.log(number1 + number2)
+    total = number1 + number2
+return total
 }
 
-sum(234, 432)
 
+let number1 = 234
+let number2 = 432
 
-
-
+console.log(`a some é ${sum(number1, number2)}`)
+console.log(number1, number2)
 
 
 
